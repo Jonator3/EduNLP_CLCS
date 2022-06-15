824b6f73d7a09a54cea446d58acd2f6ec21d2556
Jonator3 <joey.pehlke@gmail.com>
2022-06-15 14:10:52 +0200

    update 15.06.22

--- bert.py
+++ bert.py
@@ -1,36 +1,101 @@
-# verify GPU availability
-import time
-
-import tensorflow as tf
+print("imports")
+#import tensorflow as tf
 
 import torch
 import nltk
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-#from keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
+from keras.utils import pad_sequences
+#from sklearn.model_selection import train_test_split
 from pytorch_pretrained_bert import BertTokenizer, BertConfig
-from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
-from tqdm import tqdm, trange
+#from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+#from tqdm import tqdm, trange
 #import pandas as pd
-import io
-import numpy as np
-
+#import io
+#import numpy as np
 import main
 
 
+batch_size = 32
+
+print("set device")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 """
 n_gpu = torch.cuda.device_count()
 torch.cuda.get_device_name(0)
 """
 
-data = []
-for D in main.load_data("data/de.csv"):
-    sentences = [["[CLS]"]+nltk.tokenize.word_tokenize(sent)+["[SEP]"] for sent in nltk.tokenize.sent_tokenize(D.og_text)]
-    score = D.gold_score
 
-    # TODO BERT-Tokenize
-    data.append((sentences, score))
+def concat(list):
+    out = []
+    for entry in list:
+        out += entry
+    return out
 
+
+def pad_sequences(*lists):
+    pad = concat(lists)
+    padded = pad_sequences(pad, maxlen=128, dtype="long", truncating="post", padding="post")
+    i = 0
+    out = []
+    for list in lists:
+        out.append(padded[i:len(list)])
+        i += len(list)
+    return out
+
+
+def make_masks(input_ids):
+    attention_masks = []
+    # Create a mask of 1s for each token followed by 0s for padding
+    for seq in input_ids:
+        seq_mask = [float(i > 0) for i in seq]
+        attention_masks.append(seq_mask)
+    return attention_masks
+
+
+def preprocess_dataset(dataset_path, verbose=True):
+    print("loading", dataset_path)
+    data = []
+    dataset = [entry for entry in main.load_data(dataset_path) if entry.set < 3 or entry.set == 10]
+    for i, D in enumerate(dataset):
+        sentences = ["[CLS] " + sent + " [SEP]" for sent in nltk.tokenize.sent_tokenize(D.og_text)]
+        score = D.gold_score
+
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+        tokenized_text = concat([tokenizer.tokenize(sent) for sent in sentences])
+        input_ids = [tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_text]
+
+        data.append((tokenized_text, input_ids, score))
+        if verbose:
+            print("\r", i, "/", len(dataset), end="")
+    return data
+
+
+print("load and tokenize data")
+train_set = preprocess_dataset("data/en_train.csv")
+test_set = preprocess_dataset("data/en_test.csv")
+
+train_labels = [d[2] for d in train_set]
+test_labels = [d[2] for d in test_set]
+
+train_inputs, test_inputs = pad_sequences([d[1] for d in train_set], [d[1] for d in test_set])
+
+train_masks = make_masks(train_inputs)
+test_masks = make_masks(test_inputs)
+
+
+train_inputs = torch.tensor(train_inputs)
+test_inputs = torch.tensor(test_inputs)
+train_labels = torch.tensor(train_labels)
+test_labels = torch.tensor(test_labels)
+train_masks = torch.tensor(train_masks)
+test_masks = torch.tensor(test_masks)
+
+# Create an iterator of our data with torch DataLoader 
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+test_data = TensorDataset(test_inputs, test_masks, test_labels)
+test_sampler = SequentialSampler(test_data)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
 # TODO load multilingual-BERT
 # TODO train BERT
--- data_transformer.py
+++ data_transformer.py
@@ -6,47 +6,60 @@ import translator
 head = ["Id", "EssaySet", "Score", "OriginalText", "OriginalLanguage", "EnglishText", "GermanText", "SpanishText"]
 
 
-"""
-# Deutsch
-reader = csv.reader(open("data/de.csv", "r"))
-writer = csv.writer(open("data/de.csv", "w+"))
+reader1d = open("ASAP_übersetzt/train_prompt1 de.txt", "r")
+reader2d = open("ASAP_übersetzt/train_prompt2 de.txt", "r")
+reader10d = open("ASAP_übersetzt/train_prompt10 de.txt", "r")
+reader1e = open("ASAP_übersetzt/train_prompt1 es.txt", "r")
+reader2e = open("ASAP_übersetzt/train_prompt2 es.txt", "r")
+reader10e = open("ASAP_übersetzt/train_prompt10 es.txt", "r")
+reader = csv.reader(open("data/en_train.csv", "r"))
 reader.__next__()
+writer = csv.writer(open("data/en_train2.csv", "w"))
 writer.writerow(head)
-for row in reader:
-    data = row[0], row[1], row[2], row[3], "de", row[5], row[3], translator.translate(row[3], "ES")
-    time.sleep(0.1)
-    print(row[0])
-    writer.writerow(data)
 
-"""
-# Englisch
-print("en")
-reader = csv.reader(open("data/en_test.csv", "r"))
-writer = csv.writer(open("data2/en_test.csv", "w+"))
-reader.__next__()
-writer.writerow(head)
+data = {}
 for row in reader:
-    data = row[0], row[1], row[2], row[3], "en", row[5], translator.translate(row[3], "DE"), translator.translate(row[3], "ES")
-    writer.writerow(data)
-    print(row[0])
-reader = csv.reader(open("data/en_train.csvv", "r"))
-writer = csv.writer(open("data2/en_train.csv", "w"))
-reader.__next__()
-writer.writerow(head)
-for row in reader:
-    data = row[0], row[1], row[2], row[3], "en", row[5], translator.translate(row[3], "DE"), translator.translate(row[3], "ES")
-    writer.writerow(data)
-    print(row[0])
-"""
+    data[row[0]] = [row[0], row[1], row[2], row[3], row[4], row[5], "", ""]
 
-# Spanisch
-reader = csv.reader(open("data/es.csv", "r"))
-writer = csv.writer(open("data/es.csv", "w"))
-reader.__next__()
-writer.writerow(head)
-for row in reader:
-    data = row[0], row[1], row[2], row[3], "es", row[5], translator.translate(row[3], "DE"), row[3]
-    time.sleep(0.1)
-    print(row[0])
-    writer.writerow(data)
-"""
+for line in reader1d.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][6] = text
+for line in reader2d.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][6] = text
+for line in reader10d.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][6] = text
+
+for line in reader1e.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][7] = text
+for line in reader2e.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][7] = text
+for line in reader10e.readlines():
+    split = line.split(" ")
+    text = " ".join(split[5:])
+    key = "en"+split[0]
+    if data.get(key) is not None:
+        data[key][7] = text
+
+for key in data.keys():
+    row = data.get(key)
+    writer.writerow(row)
+
