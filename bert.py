c58cadbe11dd170328d9529357e716e1409cbc21
Jonator3 <joey.pehlke@gmail.com>
2022-06-27 15:52:53 +0200

    update 27.06.22

--- bert.py
+++ bert.py
@@ -7,11 +7,12 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler, Sequentia
 from keras.utils import pad_sequences
 #from sklearn.model_selection import train_test_split
 from pytorch_pretrained_bert import BertTokenizer, BertConfig
-#from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
-#from tqdm import tqdm, trange
+from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+from tqdm import tqdm, trange
 #import pandas as pd
 #import io
-#import numpy as np
+import numpy as np
+import matplotlib as plt
 import main
 
 
@@ -34,7 +35,7 @@ def concat(list):
 
 def pad_sequences(*lists):
     pad = concat(lists)
-    padded = pad_sequences(pad, maxlen=128, dtype="long", truncating="post", padding="post")
+    padded = pad_sequences(pad)
     i = 0
     out = []
     for list in lists:
@@ -52,27 +53,31 @@ def make_masks(input_ids):
     return attention_masks
 
 
-def preprocess_dataset(dataset_path, verbose=True):
+def preprocess_dataset(dataset_path, max_l=None, verbose=True):
     print("loading", dataset_path)
     data = []
     dataset = [entry for entry in main.load_data(dataset_path) if entry.set < 3 or entry.set == 10]
+    if max_l is not None:
+        dataset = dataset[:max_l]
     for i, D in enumerate(dataset):
         sentences = ["[CLS] " + sent + " [SEP]" for sent in nltk.tokenize.sent_tokenize(D.og_text)]
         score = D.gold_score
 
         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
         tokenized_text = concat([tokenizer.tokenize(sent) for sent in sentences])
-        input_ids = [tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_text]
+        input_ids = tokenizer.convert_tokens_to_ids(tokenized_text)
+        #input_ids = [tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_text]
 
         data.append((tokenized_text, input_ids, score))
         if verbose:
             print("\r", i, "/", len(dataset), end="")
+    print("\rdone\n")
     return data
 
 
 print("load and tokenize data")
-train_set = preprocess_dataset("data/en_train.csv")
-test_set = preprocess_dataset("data/en_test.csv")
+train_set = preprocess_dataset("data/en_train.csv", 100)
+test_set = preprocess_dataset("data/en_test.csv", 100)
 
 train_labels = [d[2] for d in train_set]
 test_labels = [d[2] for d in test_set]
@@ -97,5 +102,99 @@ train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batc
 test_data = TensorDataset(test_inputs, test_masks, test_labels)
 test_sampler = SequentialSampler(test_data)
 test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=4)
+model.cuda()
+
+# BERT fine-tuning parameters
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
+
+optimizer = BertAdam(optimizer_grouped_parameters,
+                     lr=2e-5,
+                     warmup=.1)
+
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+# Store our loss and accuracy for plotting
+train_loss_set = []
+# Number of training epochs
+epochs = 4
+
+# BERT training loop
+for _ in trange(epochs, desc="Epoch"):
+
+    ## TRAINING
+
+    # Set our model to training mode
+    model.train()
+    # Tracking variables
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    # Train the data for one epoch
+    for step, batch in enumerate(train_dataloader):
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+        # Clear out the gradients (by default they accumulate)
+        optimizer.zero_grad()
+        # Forward pass
+        loss = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+        train_loss_set.append(loss.item())
+        # Backward pass
+        loss.backward()
+        # Update parameters and take a step using the computed gradient
+        optimizer.step()
+        # Update tracking variables
+        tr_loss += loss.item()
+        nb_tr_examples += b_input_ids.size(0)
+        nb_tr_steps += 1
+    print("Train loss: {}".format(tr_loss / nb_tr_steps))
+
+    ## VALIDATION
+
+    # Put model in evaluation mode
+    model.eval()
+    # Tracking variables
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+    # Evaluate data for one epoch
+    for batch in test_dataloader:
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+        # Telling the model not to compute or store gradients, saving memory and speeding up validation
+        with torch.no_grad():
+            # Forward pass, calculate logit predictions
+            logits = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+            # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+    print("Validation Accuracy: {}".format(eval_accuracy / nb_eval_steps))
+
+# plot training performance
+plt.figure(figsize=(15, 8))
+plt.title("Training loss")
+plt.xlabel("Batch")
+plt.ylabel("Loss")
+plt.plot(train_loss_set)
+plt.show()
+
 # TODO load multilingual-BERT
-# TODO train BERT
