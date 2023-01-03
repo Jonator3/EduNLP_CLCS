e9e96598804d033f52348625c6ee328362300eec
Jonator3 <joey.pehlke@stud.uni-due.de>
2023-01-03 01:09:53 +0100

    update 03.01.22 -- Dataset names now contain used language

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     else:  # No subsets used
         if balance:
             trainset = balance_set(trainset)
-        result = main(trainset, kfold, testset, trainset_path, testset_path, save_model=save_model, model=model)
+        result = main(trainset, kfold, testset, trainset_path + "-" + train_lang, testset_path + "-" + test_lang, save_model=save_model, model=model)
 
     if output_path != "":  # if a path for the output is given, write to it.
         folder = "/".join(output_path.split("/")[:-1])
